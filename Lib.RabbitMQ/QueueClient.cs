34a7a5cbe96b524fccd6e09b35e2993f5df11cfb
wenhui <WenHui_Xiao@everrich.com.tw>
2023-02-18 16:09:21 +0800

    refactor : 優化邏輯

--- Lib.RabbitMQ/QueueClient.cs
+++ Lib.RabbitMQ/QueueClient.cs
@@ -11,77 +11,78 @@ namespace Lib.RabbitMQ
 {
     public class QueueClient
     {
-        private readonly QueueConnectionSettings _config;
-        private readonly ConnectionFactory _factory;
+        private QueueConnectionSettings _config;
+        private ConnectionFactory _factory;
+        private IConnection _connection;
+        private IModel _channel;
         public QueueClient(IOptions<QueueConnectionSettings> config)
         {
             _config = config.Value;
+            Connect(_config);
+        }
+
+        public void SendQueue(string queueName, string message, string exchange = "")
+        {
+
+            QueueDeclare(queueName);
+
+            var body = Encoding.UTF8.GetBytes(message);
+            var properties = _channel.CreateBasicProperties();
+            properties.Persistent = true;
+            _channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queueName,
+                mandatory: true,
+                basicProperties: properties,
+                body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+
+        }
+
+        public async Task ReceivingQueue(string queueName, string exchange = "")
+        {
+            QueueDeclare(queueName);
+
+            var consumer = new EventingBasicConsumer(_channel);
+            Console.WriteLine("等候消息中");
+            consumer.Received += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received {0} ， Time = {1}", message, DateTime.Now.ToString("mm:ss fff"));
+                await Task.Delay(1000);
+            };
+            while (true)
+            {
+                _channel.BasicConsume(queue: queueName,
+                                 autoAck: true,
+                                 consumer: consumer);
+                await Task.Delay(100);
+            }
+
+        }
+
+        private void Connect(QueueConnectionSettings settings)
+        {
             _factory = new ConnectionFactory()
             {
                 HostName = _config.HostName,
                 UserName = _config.UserName,
                 Password = _config.Password
             };
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.BasicQos(0, 1, false);
         }
-        public void SendQueue(string queueName, string message, string exchange = "")
+        private void QueueDeclare(string queueName)
         {
-
-            using (var connection = _factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                if (!string.IsNullOrEmpty(exchange))
-                {
-                    channel.ExchangeDeclare(exchange: exchange,
-                                   type: "topic");
-                }
-                channel.QueueDeclare(queue: queueName,
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-
-                var body = Encoding.UTF8.GetBytes(message);
-
-                channel.BasicPublish(String.Empty,
-                                     queueName,
-                                     true,
-                                     channel.CreateBasicProperties(),
-                                     body);
-                Console.WriteLine(" [x] Sent {0}", message);
-            }
-        }
-
-        public async Task ReceivingQueue(string queueName, string exchange = "")
-        {
-            using (var connection = _factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-
-                channel.QueueDeclare(queue: queueName,
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
-                channel.BasicQos(0, 1, false);
-
-                var consumer = new EventingBasicConsumer(channel);
-                Console.WriteLine("等候消息中");
-                consumer.Received += async(model, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine("Received {0} ， Time = {1}", message, DateTime.Now.ToString("mm:ss fff"));
-                    await Task.Delay(1000);
-                    //Console.WriteLine("Wait {0} Done， Time = {1}", message, DateTime.Now.ToString("mm:ss fff"));
-                };
-                while (true)
-                {
-                    channel.BasicConsume(queue: queueName,
-                                     autoAck: true,
-                                     consumer: consumer);
-                    await Task.Delay(100);
-                }
-            }
+            _channel.QueueDeclare(
+                queue: queueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false
+            );
         }
     }
 }
